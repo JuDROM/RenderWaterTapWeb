b88ca70de4f623be3d25dee15b4cf90f6695987d
JuDROM <jdromero@unillanos.edu.co>
2025-11-22 12:40:44 -0500

    Fixes: Conexion con backend en modo debug y produccion

--- src/main/java/com/hydro/watertap/config/CorsGlobalConfig.java
+++ src/main/java/com/hydro/watertap/config/CorsGlobalConfig.java
@@ -6,17 +6,15 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
-
 @Configuration
 public class CorsGlobalConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://watertapclone-j2hc75u1a-sierrasander29-5645s-projects.vercel.app", "http://localhost:8080", "http://localhost:3000", "https://watertapclone.vercel.app"));
-        config.setAllowedMethods(List.of("GET", "POST", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
+        config.addAllowedOriginPattern("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
